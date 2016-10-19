ad3b417326960e2871dd5e27508c918bbdcfca6b
akshaybhalotia <akshay.bhalotia@razorpay.com>
2016-10-19 17:29:55 +0530

    Moves sample app to common code

--- example/ios/example/AppDelegate.m
+++ example/ios/example/AppDelegate.m
@@ -22,9 +22,9 @@
 
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings]
-      jsBundleURLForBundleRoot:@"index.ios"
-              fallbackResource:nil];
+  jsCodeLocation =
+      [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"
+                                                     fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"example"

21ee2ae3ffe9ae758deb8efb82504b9cfe8946ea
akshaybhalotia <akshay.bhalotia@razorpay.com>
2016-10-25 14:05:13 +0530

    Change public API to use JS promise

--- RazorpayCheckout.js
+++ RazorpayCheckout.js
@@ -2,7 +2,30 @@
 
 import { NativeModules, NativeEventEmitter } from 'react-native';
 
-export const Razorpay = {
-  RazorpayCheckout: NativeModules.RazorpayCheckout,
-  RazorpayEventEmitter: NativeModules.RazorpayEventEmitter
-};
+const razorpayEvents = new NativeEventEmitter(NativeModules.RazorpayEventEmitter);
+
+class RazorpayCheckout {
+
+  static open(options, successCallback, errorCallback) {
+    return new Promise(function(resolve, reject) {
+      let removeSubscriptions = () => {
+        razorpayEvents.removeAllListeners('Razorpay::onPaymentSuccess');
+        razorpayEvents.removeAllListeners('Razorpay::onPaymentError');
+      };
+      razorpayEvents.addListener('Razorpay::onPaymentSuccess', (data) => {
+        let resolveFn = successCallback || resolve;
+        resolveFn(data);
+        removeSubscriptions();
+      });
+      razorpayEvents.addListener('Razorpay::onPaymentError', (data) => {
+        let rejectFn = errorCallback || reject;
+        rejectFn(data);
+        removeSubscriptions();
+      });
+      NativeModules.RazorpayCheckout.open(options);
+    });
+  }
+
+}
+
+export {RazorpayCheckout};
--- example/index.js
+++ example/index.js
@@ -15,45 +15,38 @@ import {
   NativeEventEmitter
 } from 'react-native';
 
-import { Razorpay } from 'react-native-razorpay';
-const { RazorpayCheckout, RazorpayEventEmitter } = Razorpay;
-
-const razorpayEvents = new NativeEventEmitter(RazorpayEventEmitter);
+import { RazorpayCheckout } from 'react-native-razorpay';
 
 class example extends Component {
-  componentWillMount() {
-    razorpayEvents.addListener('Razorpay::onPaymentSuccess', (data) => {
-      alert("Success: " + data.payment_id)
-    });
-    razorpayEvents.addListener('Razorpay::onPaymentError', (data) => {
-      alert("Error: " + data.code + " | " + data.description)
-    });
-  }
 
   render() {
     return (
       <View style={styles.container}>
        <TouchableHighlight onPress={() => {
         var options = {
-          description: 'Credits towards consultation',
-          image: 'https://i.imgur.com/3g7nmJC.png',
-          currency: 'INR',
-          key: 'rzp_test_1DP5mmOlF5G5ag',
-          amount: '5000',
-          name: 'foo',
-          prefill: {email: 'pranav@razorpay.com', contact: '8879524924', name: 'Pranav Gupta'},
-          theme: {color: '#F37254'}
+         description: 'Credits towards consultation',
+         image: 'https://i.imgur.com/3g7nmJC.png',
+         currency: 'INR',
+         key: 'rzp_test_1DP5mmOlF5G5ag',
+         amount: '5000',
+         name: 'foo',
+         prefill: {
+           email: 'akshay@razorpay.com',
+           contact: '8955806560',
+           name: 'Akshay Bhalotia'
+         },
+         theme: {color: '#F37254'}
         }
-        RazorpayCheckout.open(options)
+        RazorpayCheckout.open(options).then(data =>
+          { alert("Success: " + data.payment_id) }
+        ).catch(data =>
+          { alert("Error: " + data.code + " | " + data.description) }
+        );
        }}>
       <Text style = {styles.text}>Pay</Text>
     </TouchableHighlight>
     </View>
     );
-  }
-
-  componentWillUnmount () {
-    razorpayEvents.remove();
   }
 
 }

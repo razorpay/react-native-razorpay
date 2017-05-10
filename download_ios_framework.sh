5d90c637526deb07b5abab1b90f4c6cf44f68d2f
akshaybhalotia <akshay.bhalotia@razorpay.com>
2017-05-10 18:31:57 +0530

    Add unzip option to override prompts

--- download_ios_framework.sh
+++ download_ios_framework.sh
@@ -2,6 +2,6 @@
 ## Run this script after installing the module
 
 curl -O http://downloads.razorpay.com/Razorpay.framework-0.15.0.zip
-unzip ./Razorpay.framework-0.15.0.zip
+unzip -o ./Razorpay.framework-0.15.0.zip
 rm -r ./ios/Razorpay.framework
 cp -R ./Razorpay.framework ./ios/

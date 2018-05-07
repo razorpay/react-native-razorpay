cf8a621e44edb2a7e43473222abe9f51bbae90b5
N.A.Shashank <shashank.na@razorpay.com>
2018-05-07 14:51:10 +0530

    version 2.0.5

--- example/reload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-## Run this script after making changes to the module
-react-native unlink react-native-razorpay
-npm uninstall react-native-razorpay
-npm install
-react-native link react-native-razorpay

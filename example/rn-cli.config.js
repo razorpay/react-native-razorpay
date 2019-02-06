a46f5d8762c74caa9738f42775ec78266c703077
N.A.Shashank <shashank.na@razorpay.com>
2019-02-06 16:10:34 +0530

    added example that is built on the latest version of rn ie 0.58 and react 16.3.3

--- example/rn-cli.config.js
+++
@@ -1,7 +0,0 @@
-const blacklist = require('metro-config/src/defaults/blacklist');
-
-module.exports = {
-  resolver: {
-    blacklistRE: blacklist([/Users\/n.ashashank\/Desktop\/example\/node_modules\/react-native-razorpay\/node_modules\/.*/])
-  }
-};

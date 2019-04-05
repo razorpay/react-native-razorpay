bc6db935334b7cc86e81c724e2fa807b9c622bce
rinkuk1993 <rinkuk1993@gmail.com>
2019-04-05 20:13:41 +0530

    handled event listner

--- RazorpayCheckout.js
+++ RazorpayCheckout.js
@@ -2,9 +2,12 @@
 
 import { NativeModules, NativeEventEmitter } from 'react-native';
 
-const razorpayEvents = new NativeEventEmitter(NativeModules.RazorpayEventEmitter);
+const razorpayEvents = NativeModules.RazorpayEventEmitter ? new NativeEventEmitter(NativeModules.RazorpayEventEmitter) : null;
 
 const removeSubscriptions = () => {
+  if (!razorpayEvents) {
+    return;
+  }
   razorpayEvents.removeAllListeners('Razorpay::PAYMENT_SUCCESS');
   razorpayEvents.removeAllListeners('Razorpay::PAYMENT_ERROR');
   razorpayEvents.removeAllListeners('Razorpay::EXTERNAL_WALLET_SELECTED');
@@ -13,7 +16,7 @@ const removeSubscriptions = () => {
 class RazorpayCheckout {
   static open(options, successCallback, errorCallback) {
     return new Promise(function(resolve, reject) {
-      if (!NativeModules.RazorpayCheckout) {
+      if (!NativeModules.RazorpayCheckout || !razorpayEvents) {
         errorCallback({ code: 'RAZORPAY_NOT_INAPP', description: 'razorpay is not added in this app' });
         return;
       }
@@ -31,6 +34,9 @@ class RazorpayCheckout {
     });
   }
   static onExternalWalletSelection(externalWalletCallback) {
+    if (!razorpayEvents) {
+      return;
+    }
     razorpayEvents.addListener('Razorpay::EXTERNAL_WALLET_SELECTED', (data) => {
       externalWalletCallback(data);
       removeSubscriptions();

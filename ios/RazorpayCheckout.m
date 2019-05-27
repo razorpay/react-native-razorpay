579dcf6845fde10aa60f4546496ab5b8d8e7b94f
Atit Patel <patelatit2012@gmail.com>
2019-05-28 00:01:18 +0530

    Fix: add delay before calling open method in ios

--- ios/RazorpayCheckout.m
+++ ios/RazorpayCheckout.m
@@ -24,7 +24,7 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(open : (NSDictionary *)options) {
     
     NSString *keyID = (NSString *)[options objectForKey:@"key"];
-    dispatch_sync(dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         Razorpay *razorpay = [Razorpay initWithKey:keyID
                                andDelegateWithData:self];
         [razorpay setExternalWalletSelectionDelegate:self];

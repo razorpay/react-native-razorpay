8c2552a62530b1b07b2e59b557adcf66a5dde96b
Vivek Shindhe <vivek.shindhe@razorpay.com>
2020-07-10 10:18:44 +0530

    new app ui update

--- example/RazorpaySampleApp/App.js
+++ example/RazorpaySampleApp/App.js
@@ -1,36 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from "react";
+import {useState} from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Modal,
+  Text,
+  Button,
+  Alert,
+  TouchableHighlight,
+  Image,
+  StatusBar,
+} from 'react-native';
 
- import React, { Component } from 'react';
- import { Button, StyleSheet, View, NativeModules,StatusBar,Image,Text, NativeEventEmitter, SafeAreaView } from 'react-native';
-
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { TouchableOpacity, TextInput } from "react-native-gesture-handler";
+// import Razorpay from 'react-native-customui';
 import RazorpayCheckout from 'react-native-razorpay';
- 
+import { color } from "react-native-reanimated";
 
- export default class ButtonBasics extends Component {
+const App: () => React$Node = () =>{
+
+  
+ 
   const [modalVisible, setModalVisible] = useState(false);
   const [imageUrl, onChangeText] = React.useState('Image URL');
   const [companyName, onChangeCompName] = React.useState('Razorpay Demo');
   const [color, onChangeColor] = React.useState('#2B4486');
-  _onPressButton() {
+
+  startPayment=()=>{
+    if(imageUrl==="Image URL"){
+        onChangeText("https://i.imgur.com/3g7nmJC.png")
+    }
+
     var options = {
-        description: 'Credits towards consultation',
-        image: 'https://i.imgur.com/3g7nmJC.png',
+        name:companyName,
+        description: "Online Store",
+        image: imageUrl,
         currency: 'INR',
-        key: 'Your razorpay key',
-        amount: '5000',
-        name: 'foo',
+        key: 'rzp_live_ILgsfZCZoFIKMb',
+        amount: '100',
         prefill: {
           email: 'void@razorpay.com',
           contact: '9191919191',
           name: 'Razorpay Software'
         },
-        theme: {color: '#F37254'}
+        theme: {color: color}
       }
         RazorpayCheckout.open(options).then((data) => {
         // handle success
@@ -40,84 +62,16 @@ import RazorpayCheckout from 'react-native-razorpay';
         alert(`Error: ${error.code} | ${error.description}`);
       });
       }
- 
-//   function payWithRzp(){
-//     console.log(companyName)
-//     if (imageUrl==="Razorpay Demo") {
-//           imageUrl = "https://s3.amazonaws.com/rzp-mobile/images/rzp.png";   
-//         }
-//         var options = {
-//           name:companyName,
-          
-//           image:imageUrl,
-//           description: 'Credits towards consultation',
-//           currency: 'INR',
-//           theme:{
-//             color:this.color
-//           },
-//           key_id: 'rzp_test_1DP5mmOlF5G5ag',
-//           amount: '100',
-//           email: 'gaurav.kumar@example.com',
-//           contact: '9123456789',
-          
-//        }
-//        console.log(options.key_id);
-       
-//        RazorpayCheckout.open(options).then((data) => {
-//           // handle success
-//           alert(`Success: ${data.razorpay_payment_id}`);
-//       }).catch((error) => {
-//           // handle failure
-//           alert(`Error: ${error.code} | ${error.description}`);
-//          });
-//   }
-//   var options = {
-//     description: 'Credits towards consultation',
-//     image: 'https://i.imgur.com/3g7nmJC.png',
-//     currency: 'INR',
-//     key: 'rzp_test_1DP5mmOlF5G5ag',
-//     amount: '5000',
-//     name: 'foo',
-//     prefill: {  _onPressButton() {
-// var options = {
-//     description: 'Credits towards consultation',
-//     image: 'https://i.imgur.com/3g7nmJC.png',
-//     currency: 'INR',
-//     key: 'Your razorpay key',
-//     amount: '5000',
-//     name: 'foo',
-//     prefill: {
-//       email: 'void@razorpay.com',
-//       contact: '9191919191',
-//       name: 'Razorpay Software'
-//     },
-//     theme: {color: '#F37254'}
-//   }
-//     RazorpayCheckout.open(options).then((data) => {
-//     // handle success
-//     alert(`Success: ${data.razorpay_payment_id}`);
-//   }).catch((error) => {
-//     // handle failure
-//     alert(`Error: ${error.code} | ${error.description}`);
-//   });
-//   }
-//       email: 'void@razorpay.com',
-//       contact: '9191919191',
-//       name: 'Razorpay Software'
-//     },
-//     theme: {color: '#F37254'}
-//   }
-
+  
   return(
     <>
-    
-    
     <Modal
         animationType="slide"
         transparent={false}
         visible={modalVisible}
         onRequestClose={() => {
           // Alert.alert("Modal has been closed.");
+          console.log(imageUrl);
         }}>
         <ScrollView>
        <View style={styles.header_container}>
@@ -258,7 +212,7 @@ import RazorpayCheckout from 'react-native-razorpay';
 
 <StatusBar barStyle="dark-content" />
 
-      <><View style={styles.header_container}>
+      <View style={styles.header_container}>
         <View style={styles.logo_header}>
           <Image style={styles.razorpay_logo}
             source={require('./assets/images/Logo.png')}
@@ -369,9 +323,7 @@ import RazorpayCheckout from 'react-native-razorpay';
           <Text style={{ alignSelf:"flex-end",fontFamily:"lato_bold", fontSize:20,color:"#162F56" }}>₹1.00</Text>          
 
         </View>
-        <TouchableHighlight activeOpacity = { .5 } onPress={() => {
-               _onPressButton
-              }}> 
+        <TouchableHighlight activeOpacity = { .5 } onPress={() => {startPayment()}}> 
         <Text  style={{alignSelf:"center",backgroundColor:"#2B83EA",marginBottom:32, marginStart:16,marginEnd:16,paddingTop:10,paddingBottom:10,width:"80%",textAlign:"center",
             fontSize:14,fontFamily:"lato_bold",color:"#ffffff"}}>
           Pay Now
@@ -471,3 +423,36 @@ const styles = StyleSheet.create({
     
 });
 
+function payWithRazorpay(){
+ 
+//   finalImageUrl :String = "https://s3.amazonaws.com/rzp-mobile/images/rzp.png"
+//   if (this.imageUrl!=="Razorpay Demo") {
+//       finalImageUrl = imageUrl;
+//   }
+//   var options = {
+//     name:companyName,
+    
+//     image:finalImageUrl,
+//     description: 'Credits towards consultation',
+//     currency: 'INR',
+//     theme:{
+//       color:this.color
+//     },
+//     key_id: 'rzp_test_1DP5mmOlF5G5ag',
+//     amount: '100',
+//     email: 'gaurav.kumar@example.com',
+//     contact: '9123456789',
+    
+//  }
+ 
+//  RazorpayCheckout.open(options).then((data) => {
+//     // handle success
+//     alert(`Success: ${data.razorpay_payment_id}`);
+// }).catch((error) => {
+//     // handle failure
+//     alert(`Error: ${error.code} | ${error.description}`);
+//    });
+}
+
+
+export default App;

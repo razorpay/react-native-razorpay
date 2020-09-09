bb3e12e5bf5727dacbad2dd586d3ff0df4c6f078
Tushar Korde <tushar.korde@biz4solutions.com>
2020-09-09 12:17:13 +0530

    presenting razorpay vc from root view controller of the application

--- ios/RazorpayCheckout.m
+++ ios/RazorpayCheckout.m
@@ -30,11 +30,22 @@ RCT_EXPORT_METHOD(open : (NSDictionary *)options) {
                                andDelegateWithData:self];
         [razorpay setExternalWalletSelectionDelegate:self];
         
-  NSMutableDictionary * tempOptions = [[NSMutableDictionary alloc] initWithDictionary:options];
-//   tempOptions[@"integration_version"] = [self findReactNativeVersion];
-  tempOptions[@"integration"] = @"react-native";
-  tempOptions[@"FRAMEWORK"] = @"react-native";
-  [razorpay open:tempOptions];
+        NSMutableDictionary * tempOptions = [[NSMutableDictionary alloc] initWithDictionary:options];
+        //tempOptions[@"integration_version"] = [self findReactNativeVersion];
+        tempOptions[@"integration"] = @"react-native";
+        tempOptions[@"FRAMEWORK"] = @"react-native";
+        
+        //get root view to present razorpay vc
+        id<UIApplicationDelegate> app = [[UIApplication sharedApplication] delegate];
+        UINavigationController *rootViewController = ((UINavigationController*) app.window.rootViewController);
+        
+        if (rootViewController.presentedViewController) {
+            [razorpay open:tempOptions displayController:rootViewController.presentedViewController];
+            return;
+        }
+        
+        //Use 'open' method with displayController parameter
+        [razorpay open:tempOptions displayController:rootViewController];
     });
 }
 
